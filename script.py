43e5719cf256c283d19fe021467df74e0bc17c1f
piethrower <piethrowerchamp@gmail.com>
2021-04-21 13:47:20 -0400

    Clean code

--- script.py
+++ script.py
@@ -5,10 +5,14 @@ import time
 import asyncio
 import VoiceUser
 import secrets
+
 client = discord.Client()
 active_users = list()
 bopping = False
+
 sleep_time = 300
+
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -16,34 +20,34 @@ async def on_ready():
         await asyncio.sleep(sleep_time)
         await check_for_bop()
 
+
 @client.event
 async def on_voice_state_update(member, before, after):
     global active_users
     temp = VoiceUser.Voice_User()
     if member != client.user:
-        if after.channel != None:
-            if before.channel != None:
-                for vu in active_users:
-                    if member == vu.user:
-                        active_users.remove(vu)
+        if after.channel and before.channel:
+            for vu in active_users:
+                if member == vu.user:
+                    active_users.remove(vu)
             temp.user = member
             temp.channel = after.channel
             active_users.append(temp)
             for vu in active_users:
-                if vu.channel.name == after.channel.name:
-                    if len(after.channel.members)==1:
-                        if not vu.is_alone:
-                            temp.time_alone_start = time.perf_counter()
-                    vu.is_alone = (len(after.channel.members)==1)
+                if vu.channel.name == after.channel.name and len(after.channel.members) == 1:
+                    if not vu.is_alone:
+                        temp.time_alone_start = time.perf_counter()
+                    vu.is_alone = (len(after.channel.members) == 1)
         else:
             for vu in active_users:
                 if member == vu.user:
-                        active_users.remove(vu)
+                    active_users.remove(vu)
                 if vu.channel.name == before.channel.name:
-                    if len(before.channel.members)==1:
-                        if not vu.is_alone:
-                            temp.time_alone_start = time.perf_counter()
-                    vu.is_alone = (len(after.channel.members)==1) ## throwing errors when last user leaves
+                    if len(before.channel.members) == 1 and vu.is_alone:
+                        temp.time_alone_start = time.perf_counter()
+                    # throwing errors when last user leaves
+                    vu.is_alone = (len(after.channel.members) == 1)
+
 
 async def check_for_bop():
     for vu in active_users:
@@ -53,11 +57,13 @@ async def check_for_bop():
         if time_alone >= sleep_time and not bopping:
             await bop(vu.channel)
 
+
 async def bop(VC: discord.VoiceChannel):
     global bopping
     bopping = True
     boppage = await VC.connect()
-    boppage.play(discord.FFmpegPCMAudio(source=r"/home/pi/Documents/Discord/ImportantBot/audio.mp3"))
+    boppage.play(discord.FFmpegPCMAudio(
+        source=r"/home/pi/Documents/Discord/ImportantBot/audio.mp3"))
     while boppage.is_playing():
         await asyncio.sleep(.1)
     await boppage.disconnect()
